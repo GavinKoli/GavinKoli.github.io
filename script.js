f0c3dd4e1fa00213a30f977c9334966685cc496c
Gavin Koli <92108920+GavinKoli@users.noreply.github.com>
2024-10-04 15:57:15 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.getElementById('nav-toggle');
-    const navLinks = document.getElementById('nav-links');
-
-    // Function to toggle the visibility of the navigation links
-    navToggle.addEventListener('click', function () {
-        navLinks.classList.toggle('active');
-    });
-});
